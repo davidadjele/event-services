543fba188c432f70fb908df012bb86cc3d7c3eb1
davidadjele <adjelek.dav@gmail.com>
2026-02-17 21:39:37 -0500

    Integrate Spotless for code formatting and enforce rules in build pipeline

--- src/main/java/com/event/services/EventServicesApplication.java
+++ src/main/java/com/event/services/EventServicesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventServicesApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(EventServicesApplication.class, args);
+  }
 }
